adf8eb464028d2c8532e0124aae1600d3db0fb25
pyo-sh <pygosky@gmail.com>
2026-04-10 17:56:02 +0900

    fix: scope public header to public layout (#297)

--- src/app-layer/provider/index.tsx
+++ src/app-layer/provider/index.tsx
@@ -4,7 +4,6 @@ import { PropsWithChildren, useEffect } from "react";
 import { QueryProvider } from "@app-layer/provider/query-provider";
 import { ToastProvider } from "@app-layer/provider/toast-provider";
 import { ThemeProvider } from "@app-layer/theme";
-import { Header } from "@widgets/header";
 
 interface IProps extends PropsWithChildren {
   initialTheme: string;
@@ -32,7 +31,6 @@ export default function Providers({ children, initialTheme }: IProps) {
   return (
     <QueryProvider>
       <ThemeProvider initialTheme={initialTheme}>
-        <Header />
         {children}
         <ToastProvider />
       </ThemeProvider>
